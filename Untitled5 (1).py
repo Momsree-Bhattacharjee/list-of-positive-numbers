430764f5d4f0915ad02c41e159edb80f97ec57a5
Momsree-Bhattacharjee <90470723+Momsree-Bhattacharjee@users.noreply.github.com>
2021-09-13 13:13:48 +0530

    Add files via upload

---
+++ Untitled5 (1).py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+list1 = [54, 33, -74, -58] 
+
+for numbers in list1 :
+    if numbers > 0 :
+        print ("Positive numbers are :");
+        print (numbers);
+    else:
+        print ("none");
+list2 = [ 12 , 68 , 54 , 94 , 13]
+
+for numbers in list2:
+    if numbers > 0 :
+        print ("positive numbers are:");
+        print (numbers);
+    else:
+        print ("nil");
+
+
+# In[ ]:
+
+
+
+
